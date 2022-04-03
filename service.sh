43b327d9f33e1f47bcc30afcb22477e01798f440
Taka <takeru.kageyuki@gmail.com>
2022-04-03 17:18:51 +0700

    Update service.sh

--- service.sh
+++ service.sh
@@ -7,6 +7,22 @@ iptables -t nat -A OUTPUT -p udp --dport 53 -j DNAT --to-destination :53
 iptables -t nat -I OUTPUT -p tcp --dport 53 -j DNAT --to-destination :53
 iptables -t nat -I OUTPUT -p udp --dport 53 -j DNAT --to-destination :53
 
+# Internet Tweak
+echo "0" > /proc/sys/net/ipv4/tcp_timestamps
+echo "1" > /proc/sys/net/ipv4/tcp_sack
+echo "1" > /proc/sys/net/ipv4/tcp_tw_recycle
+echo "1" > /proc/sys/net/ipv4/tcp_tw_reuse
+echo "1" > /proc/sys/net/ipv4/tcp_window_scaling
+echo "1048576" > /proc/sys/net/core/rmem_max
+echo "1048576" > /proc/sys/net/core/wmem_max
+echo "262144" > /proc/sys/net/core/rmem_default
+echo "262144" > /proc/sys/net/core/wmem_default
+echo "30" > /proc/sys/net/ipv4/tcp_fin_timeout
+echo "30" > /proc/sys/net/ipv4/tcp_keepalive_intvl
+echo "5" > /proc/sys/net/ipv4/tcp_keepalive_probes
+echo "6144,16384,1048576" > /proc/sys/net/ipv4/tcp_wmem
+echo "6144,87380,1048576" > /proc/sys/net/ipv4/tcp_rmem
+
 # Virtual Memory Tweak
 stop perfd
 echo '0' > /proc/sys/vm/oom_kill_allocating_task
