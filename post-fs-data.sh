4b3ae0a5a72d176bafdf57268f0ccb72d112f266
Taka <takeru.kageyuki@gmail.com>
2022-04-05 12:01:17 +0700

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -2,17 +2,17 @@
 MODDIR=${0%/*}
 
 # DNS Changer
-resetprop net.dns1
-resetprop net.dns2
-resetprop net.eth0.dns1
-resetprop net.eth0.dns2
-resetprop net.ppp0.dns1
-resetprop net.ppp0.dns2
-resetprop net.rmnet0.dns1
-resetprop net.rmnet0.dns2
-resetprop net.rmnet1.dns1
-resetprop net.rmnet1.dns2
-resetprop net.pdpbr1.dns1
-resetprop net.pdpbr1.dns2
-resetprop net.lte.dns1
-resetprop net.lte.dns2
+resetprop -n net.dns1
+resetprop -n net.dns2
+resetprop -n net.eth0.dns1
+resetprop -n net.eth0.dns2
+resetprop -n net.ppp0.dns1
+resetprop -n net.ppp0.dns2
+resetprop -n net.rmnet0.dns1
+resetprop -n net.rmnet0.dns2
+resetprop -n net.rmnet1.dns1
+resetprop -n net.rmnet1.dns2
+resetprop -n net.pdpbr1.dns1
+resetprop -n net.pdpbr1.dns2
+resetprop -n net.lte.dns1
+resetprop -n net.lte.dns2
