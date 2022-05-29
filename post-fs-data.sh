94b873652777f30c6a5aa79096bf17353ee68c85
Yudha ID <99653489+alicee98@users.noreply.github.com>
2022-05-29 19:41:29 +0700

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -15,9 +15,3 @@ rm -rf /storage/emulated/0/Android/data/*/cache/*
 rm -rf /data/data/*/cache/*
 rm -rf /data/dalvik-cache/arm/*
 rm -rf /data/dalvik-cache/arm64/*
-rm -rf /cache/magisk.log
-rm -rf /cache/magisk.log.bak
-touch /cache/magisk.log
-touch /cache/magisk.log.bak
-chmod 644 /cache/magisk.log
-chmod 644 /cache/magisk.log.bak
